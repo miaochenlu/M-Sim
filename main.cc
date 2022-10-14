9cd9fb7b5abd9262c143b64de456124db0dedb31
miaochenlu <3170102668@zju.edu.cn>
2022-10-14 17:24:27 +0800

    init rust

--- main.cc
+++
@@ -1,9 +0,0 @@
-#include "processor.h"
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    processor m_proc;
-    m_proc.run();
-}
--- run.sh
+++
@@ -1,1 +0,0 @@
-LD_LIBRARY_PATH=/usr/local/systemc-2.3.3/lib-linux64/ ./build/simulator
